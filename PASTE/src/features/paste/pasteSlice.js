b51912f2cf604f0e39a7943d40b7ad0b106ae493
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2024-10-03 23:20:50 +0530

    PASTE paste-Creating logic added

--- PASTE/src/features/paste/pasteSlice.js
+++ PASTE/src/features/paste/pasteSlice.js
@@ -1,29 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
 const initialState = {
-  value: 0,
-}
+  pastes: localStorage.getItem("pastes")
+    ? JSON.parse(localStorage.getItem("pastes"))
+    : [],
+};
 
 export const pasteSlice = createSlice({
-  name: 'paste',
+  name: "paste",
   initialState,
   reducers: {
     createPaste: (state, action) => {
-      state.value += action.payload
+      const paste = action.payload;
+      const newPasteTitle = paste.title;
+      const newPasteContent = paste.content;
+      // Check if the title or content is empty
+      if (!newPasteTitle.trim() || !newPasteContent.trim()) {
+        toast("Title and content cannot be empty! 🙁");
+        return; // Exit the function to prevent storing the paste
+      }
+      const pastesArray = JSON.parse(localStorage.getItem("pastes")) || [];
+      // And only checking for the newPaste is already existes if pasteArray is not empty
+      if (pastesArray.length > 0) {
+        // Checking if the paste already exists or not!
+        // Using:- Indexing with Map for Fast-Lookup
+        const pastesMap = new Map(
+          pastesArray.map((paste) => [paste.title, paste])
+        );
+        if (pastesMap.has(newPasteTitle)) {
+          toast("Paste Already Exists 🙁");
+        } else {
+          state.pastes.push(paste);
+          localStorage.setItem("pastes", JSON.stringify(state.pastes));
+          toast("Paste Created Successfully 🥳");
+        }
+      } else {
+        state.pastes.push(paste);
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+        toast("Paste Created Successfully 🥳");
+      }
     },
-    removePaste: (state, action) => {
-      state.value += action.payload
-    },
-    updatePaste: (state, action) => {
-      state.value += action.payload
-    },
-    resetAllPastes: (state, action) => {
-      state.value += action.payload
-    },
+    removePaste: (state, action) => {},
+    updatePaste: (state, action) => {},
+    resetAllPastes: (state, action) => {},
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { createPaste, removePaste, updatePaste, resetAllPastes  } = pasteSlice.actions
+export const { createPaste, removePaste, updatePaste, resetAllPastes } =
+  pasteSlice.actions;
 
-export default pasteSlice.reducer
+export default pasteSlice.reducer;
--- PASTE/src/main.jsx
+++ PASTE/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './main.css'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </StrictMode>,
 )
