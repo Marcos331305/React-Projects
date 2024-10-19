a8a330db8e22785333b9309ab7ce047b26b5f10d
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2024-10-19 16:10:52 +0530

    TalKer react router configured

--- TalKer/src/main.jsx
+++ TalKer/src/main.jsx
@@ -4,9 +4,21 @@ import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <Provider store={store}>
       <CssBaseline />
       <App />
